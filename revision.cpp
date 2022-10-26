d9d977c7a34dda674a3f9f07d38146f448ad9c7e
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-10-26 19:46:11 -0300

    Update revision.cpp

--- revision.cpp
+++ revision.cpp
@@ -1,31 +1,26 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Twits"
 
-void nombredecallback(const std_msgs::String &msg)
-{
-  ROS_INFO("mensaje");
-  //se hace algo con los datos de la subcripcion
+geometry_msgs::Twist msg;
+nav_msgs::Odometry cam;
+
+
+void posicion(const nav_msgs::Odometry &pos){
+  cam=pos;
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "subscriptor_publicador");
   ros::NodeHandle nh;
+  ros::Subscriber subcripcion = nh.subscribe("/RosAria/pose", 1000, posicion);
 
-  ros::Subscriber subcripcion = nh.subscribe("nombre del topico", 1000, nombredecallback);
-  ros::spin();
-  ros::Publisher publicacion = nh.advertise<geometry_msgs::Twist>("nombre topico",1000);
+  ros::Publisher publicacion = nh.advertise<nav:<nav_msgs::Odometry>("/revision",1000);
   ros::Rate rate(10);
-  geometry_msgs::Twist nombremensaje;
-  chatter_pub.publish(nombredemensaje);
   while (ros::ok()){
-      geometry_msgs::Twist nombredemensaje;
-      msg.linear.x=1;
-      msg.angular.z=2;
-      chatter_pub.publish(nombredemensaje);
-      ROS_INFO("estoy publicandooo");
-      rate.sleep();
-      //en este sector se publica
+      ros::spinOnce();
+    
   }
 
   return 0;
