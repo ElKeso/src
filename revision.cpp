50c8fd6bd3c696a373112fa6271c50dfe8e3599d
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-10-27 01:04:52 -0300

    Update revision.cpp

--- revision.cpp
+++ revision.cpp
@@ -17,24 +17,24 @@ float conv(float z, float w){
         if(w<=1 && w>=0.7){
             r_min=1; r_max=0.7; rad_min=0; rad_max=PI/2;
             m=(rad_max-rad_min)/(r_max-r_min);
-            v_c=m*(w-r_min)-rad_min;
+            v_c=m*(w-r_min)+rad_min;
         }
         if(w<0.7 && w>=0){
             r_min=0.7; r_max=0; rad_min=PI/2; rad_max=PI;
             m=(rad_max-rad_min)/(r_max-r_min);
-            v_c=m*(w-r_min)-rad_min;  
+            v_c=m*(abs(w)-r_min)+rad_min;  
         }
     }
     else{
         if(abs(w)<=1 && abs(w)>=0.7){
             r_min=1; r_max=0.7; rad_min=0; rad_max=-PI/2;
             m=(rad_max-rad_min)/(r_max-r_min);
-            v_c=m*(w-r_min)-rad_min;
+            v_c=m*(abs(w)-r_min)+rad_min;
         }  
         if(abs(w)<0.7 && abs(w)>=0){
             r_min=0.7; r_max=0; rad_min=-PI/2; rad_max=-PI;
             m=(rad_max-rad_min)/(r_max-r_min);
-            v_c=m*(w-r_min)-rad_min;
+            v_c=m*(abs(w)-r_min)+rad_min;
         }  
     }   
   return v_c;
