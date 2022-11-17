6db6f76acce43fd3ccdfcdda72c4bb3b07112731
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-11-17 14:54:36 -0300

    Update freno.cpp

--- freno.cpp
+++ freno.cpp
@@ -2,7 +2,7 @@
 #include "geometry_msgs/Twist.h"
 
 
-int flag=0; 
+int flag=0; int a=4;
 geometry_msgs::Twist mover;
 
 int main(int argc, char **argv){
@@ -13,19 +13,18 @@ int main(int argc, char **argv){
   while (ros::ok()){
     switch(flag){
         case 0:
-            for(int i=0; i<31; i=i+1){
-                mover.linear.x=1;
+            for(int i=0; i=15; i=i+1){
+                mover.linear.x=4;
                 pub.publish(mover);
                 loop_rate.sleep();
             }
             flag=1;
         break;
         case 1:
-            for(int i=0; i<=10; i=i+1){
-                mover.linear.x=-1;
+            for(int i=0; i=5; i=i+1){
+                mover.linear.x=-a;
                 pub.publish(mover);
-                mover.linear.x=mover.linear.x+0.1;
-                if(mover.linear.x==0){
+                if(i==5){
                     ros::shutdown();
                 }
             }
