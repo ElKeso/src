f11cbafcb8b540f9ce597d7a7c638a2194a07234
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-11-17 13:43:52 -0300

    Update freno.cpp

--- freno.cpp
+++ freno.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv){
   while (ros::ok()){
     switch(flag){
         case 0:
-            for(int i=0; i<=10; i=i+1){
+            for(int i=0; i<31; i=i+1){
                 mover.linear.x=1;
                 pub.publish(mover);
                 loop_rate.sleep();
