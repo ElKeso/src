87f08f5a3d94c2bf65a29b3c0520d9ffc860339a
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-11-18 12:49:14 -0300

    Update reloj.cpp

--- reloj.cpp
+++ reloj.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
   int cont=0;
   ros::Rate loop_rate(5);
   while (ros::ok()){
-    if(cont==30){
+    if(cont==50){
         r.ret=1;
         r.datos=cont;
         pub_ret.publish(r);
