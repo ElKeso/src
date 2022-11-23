42c8787f4e1351e90af64f25be89685d3b7ebcd5
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-11-23 11:36:36 -0300

    Update retorno.cpp

--- retorno.cpp
+++ retorno.cpp
@@ -187,12 +187,13 @@ int main(int argc, char **argv){
         //funcion para rotar
         if(abs(eu_angular(x, y)-conv(z, w))>t_a){
           mover.linear.x=0;
-          if(eu_angular(x, y)-conv(cam.pose.pose.orientation.z, cam.pose.pose.orientation.w)>0){
-            mover.angular.z=0.2;
-          }
-          else {
-            mover.angular.z=-0.2;
-          } 
+          mover.angular.z=0.2;
+          //if(eu_angular(x, y)-conv(cam.pose.pose.orientation.z, cam.pose.pose.orientation.w)>0){
+          //  mover.angular.z=0.2;
+          //}
+          //else {
+          //  mover.angular.z=-0.2;
+          //} 
         }
         else{
           //frenar...
