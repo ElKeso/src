ad573da162c6da4cc5973ec37eb2991d791333b9
ElKeso <115960132+ElKeso@users.noreply.github.com>
2022-11-23 14:58:41 -0300

    Update retorno.cpp

--- retorno.cpp
+++ retorno.cpp
@@ -274,7 +274,7 @@ int main(int argc, char **argv){
      //hacer default para no hacer nada y esperar denuevo otro retorno
     }
 //
-    ROS_INFO("Estoy en la secuencia %d, voy en %d y coordenadas son x=%f y=%f y el angulo es %f" , stop, c, x, y, eu_angular(x, y));
+    ROS_INFO("Estoy en la secuencia %d, voy en %d y coordenadas son x=%f y=%f y el angulo es %f" , stop, c, x, y, cam.pose.pose.orientation.w);
     loop_rate.sleep();
     }
   }
